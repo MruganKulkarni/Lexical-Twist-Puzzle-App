838089db54a3d6898e5ddf7eb623c72cd07bfa87
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-02-13 16:05:42 +0530

    [Mrugan Kulkarni] : UC2 - Accept two words from user

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class LexicalTwistPuzzle {
 
     public static void main(String[] args) {
+
         System.out.println("Lexical Twist Puzzle");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first word: ");
+        String word1 = scanner.next();
+
+        System.out.print("Enter second word: ");
+        String word2 = scanner.next();
+
+        System.out.println("First Word: " + word1);
+        System.out.println("Second Word: " + word2);
+
+        scanner.close();
     }
 }
